fe0d12cb6fb19884993c987d68015d82a40aae62
rnjsrntkd95 <rnjsrntkd95@gmail.com>
2019-08-07 03:46:16 +0900

    Bugfix

--- App.js
+++ App.js
@@ -1,77 +1,75 @@
 import React, { Component } from 'react';
 
 import { 
-  createStackNavigator,
-  createAppContainer,
-  createBottomTabNavigator
-} from 'react-navigation';
+    createStackNavigator,
+    createAppContainer,
+    createBottomTabNavigator,
+} from 'react-navigation'
 
 import SplashScreen from 'react-native-splash-screen'
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/FontAwesome'
 
-import RestaurantList from 'components/RestaurantList';
-import RestaurantInfo from 'components/RestaurantInfo';
-import About from 'components/About';
-import AddReview from 'components/AddReview';
+import RestaurantList from 'components/RestaurantList'
+import RestaurantInfo from 'components/RestaurantInfo'
+import About from 'components/About'
+import AddReview from 'components/AddReview'
 
 const List = createStackNavigator({
-  Home: { screen: RestaurantList},
-  Info: { screen: RestaurantInfo }
-}, {
-  defaultNavigationOptions: {
-     headerStyle: { 
-        backgroundColor: '#0066CC',
-        color: '#FFF'
-    },
-    headerTintColor: '#FFF',
-    headerTitleStyle: {
-       color: '#FFF'
-    },
-  }
-});
-
-const Tabs = createBottomTabNavigator({
-  List: { screen: List },
-  About: { screen: About }
-}, {
-  defaultNavigationOptions: ({ navigation }) => {
-    return {
-      tabBarIcon: ({ tintColor }) => {
-        const route = navigation.state.routeName;
-        console.log('route', route);
-        const name = {
-          'List': 'list',
-          'About': 'info-circle'
-        }[route]
-        return <Icon name={name} color={tintColor} size={22} />
-      },
-      tabBarOptions: {
-        activeBackgroundColor: '#E6F0FA'
+    Home: { screen: RestaurantList},
+    Info: { screen:  RestaurantInfo}
+  }, {
+      defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: '#0066CC',
+            color: '#FFF'
+        },
+        headerTintColor: '#FFF',
+        headerTitleStyle: {
+            color: '#FFF'
+        }
       }
-    }
-  }
-});
-
-const ModalNav = createStackNavigator({
-  Tabs: { screen: Tabs },
-  AddReview: { screen: AddReview }  
-}, {
-  mode: 'modal',
-  headerMode: 'none',
-  navigationOptions: {
-    gesturesEnabled: false
-  }
-});
-
-const AppContainer = createAppContainer(ModalNav);
-
-export default class App extends Component{
-
-  componentDidMount() {
-    SplashScreen.hide()
-  }
+  });
   
-  render() {
-    return <AppContainer />
-  }
-};
+  const Tabs = createBottomTabNavigator({
+      List: { screen: List },
+      About: { screen: About }
+  }, {
+      defaultNavigationOptions: ({ navigation }) => {
+          return {
+              tabBarIcon: ({ tintColor }) => {
+                  const route = navigation.state.routeName
+                  const name = {
+                      'List' : 'list',
+                      'About' : 'info-circle'
+                  }[route]
+                  return <Icon name = {name} color = {tintColor} size = {22} />
+              },
+              tabBarOptions: {
+                  activeBackgroundColor: '#E6F0FA'
+              }
+          }
+      }
+  })
+
+  const ModalNav = createStackNavigator({
+      Tabs: { screen: Tabs },
+      AddReview: { screen: AddReview }
+  }, {
+      mode: 'modal',
+      headerMode: 'none',
+      navigationOptions: {
+          gesturesEnabled: false
+      }
+  })
+  
+  const AppContainer = createAppContainer(ModalNav);
+  
+  export default class App extends Component {
+    componentDidMount() {
+        SplashScreen.hide()
+    }
+
+    render() {
+        return <AppContainer />
+    }
+}
