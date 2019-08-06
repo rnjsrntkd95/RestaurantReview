13194ab150bdd67d2caf0ecc5cdbc23cbd6b5109
rnjsrntkd95 <rnjsrntkd95@gmail.com>
2019-08-07 03:28:53 +0900

    init tutorials

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import {
+  View,
+  Text,
+} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Restaurtant Review</Text>
+      </View>
+    )
+  };
+};
